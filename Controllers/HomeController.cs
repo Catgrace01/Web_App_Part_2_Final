b58eb1c5659d203469292728c3088774b207bf2a
Grace Hammond <catgrace01@gmail.com>
2021-05-16 20:37:52 -0500

    Created program and added required actions

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web_App_Part_2_Final.Models;
+
+namespace Web_App_Part_2_Final.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index(){
+            string html = "<form method='post'>" + "<input type ='text' name='name' />" + "<input type ='submit' value='Greet me!' />" + "</form>";
+
+            return Redirect("/Hello/Goodbye");
+        }
+        // /Hello
+        [Route("/Hello")]
+        [HttpPost]
+        public IActionResult Display(string name = "World")
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+        
+        //Handle /Hello/Chris Requests
+        [Route("/Hello/{name}")]
+        public IActionResult Index2(string name)
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye!")
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
